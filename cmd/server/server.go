f70b4ad5349965343d9529f43354e4a7401eac76
Germain GAU <germain.gau@c-s.fr>
2019-01-24 13:43:41 +0100

    Enabled config file + env argument parsing

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,7 +2,11 @@ package server
 
 import (
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
+
+	"os/user"
 
 	"github.com/rs/zerolog/log"
 
@@ -15,44 +19,79 @@ type apiFlags struct {
 	BindAddr      string
 	BindPort      uint16
 	EtcdEndpoints []string
+	Config        string
+}
+
+// Splits {"a,b", "c"} into {"a", "b", "c"}
+// Temporary fix (hopefully) because Cobra doesn't
+// handle separators if they are not followed by a whitespace.
+func splitParts(maybeParted []string) []string {
+	r := []string{}
+	for _, x := range maybeParted {
+		if strings.Contains(x, ",") {
+			for _, newKey := range strings.Split(x, ",") {
+				r = append(r, newKey)
+			}
+		} else {
+			r = append(r, x)
+		}
+	}
+	return r
 }
 
 func parseArgs(flags *apiFlags) func() {
 	return func() {
-		flags.BindAddr = viper.Get("addr").(string)
+
+		flags.BindAddr = viper.GetString("addr")
 		port, err := strconv.ParseUint(viper.Get("port").(string), 10, 16)
 		if err != nil {
 			log.Fatal().
 				Str("port", viper.Get("addr").(string)).
 				Msg(fmt.Sprintf("Could not parse %s as an integer.", viper.Get("addr").(string)))
 		}
+		flags.EtcdEndpoints = splitParts(viper.GetStringSlice("etcd"))
 		flags.BindPort = uint16(port)
 	}
 }
 
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			log.Fatal().Str("error", err.Error()).Msg("Could not find user home directory")
+func initConfig(flags *apiFlags) func() {
+	return func() {
+		cnf := viper.GetString("config")
+		if cnf != "" {
+			viper.SetConfigFile(cnf)
+		} else {
+			cwd, err := os.Getwd()
+			if err != nil {
+				log.Warn().
+					Str("error", err.Error()).
+					Msg("Ignoring current directory as config file source.")
+			} else {
+				viper.AddConfigPath(cwd)
+			}
+
+			user, err := user.Current()
+			if err != nil {
+				log.Warn().
+					Str("error", err.Error()).
+					Msg("Could not find current user informations, ignoring configuration file")
+				return
+			}
+			viper.AddConfigPath(user.HomeDir)
+			viper.SetConfigName(".rssh")
 		}
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".")
-	}
-	viper.AutomaticEnv() // read in environment variables that match
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+
+		if err := viper.ReadInConfig(); err == nil {
+			log.Info().Str("file", viper.ConfigFileUsed()).Msg("Configuration file loaded")
+		} else {
+			log.Warn().Str("error", err.Error()).Msg("Could not load configuration file.")
+		}
 	}
 }
 
 func NewCommand() *cobra.Command {
 	flags := &apiFlags{}
 
+	cobra.OnInitialize(initConfig(flags))
 	cobra.OnInitialize(parseArgs(flags))
 	cmd := &cobra.Command{
 		Use:   "server",
@@ -67,7 +106,7 @@ func NewCommand() *cobra.Command {
 				return err
 			}
 
-			executor, err := api.NewExecutor([]string{"http://127.0.0.1:2379"})
+			executor, err := api.NewExecutor(flags.EtcdEndpoints)
 			if err != nil {
 				return err
 			}
@@ -89,6 +128,22 @@ func NewCommand() *cobra.Command {
 		"p",
 		8080,
 		"HTTP API port",
+	)
+
+	cmd.PersistentFlags().StringSliceVarP(
+		&flags.EtcdEndpoints,
+		"etcd",
+		"e",
+		[]string{"http://127.0.0.1:2379"},
+		"Comma separated list of the Etcd hosts to discover",
+	)
+
+	cmd.PersistentFlags().StringVarP(
+		&flags.Config,
+		"config",
+		"c",
+		"",
+		"Server configuration file to use",
 	)
 
 	viper.BindPFlags(cmd.PersistentFlags())
