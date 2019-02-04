8952736df52f42fd64685e1c2aac123e9db499cf
Germain GAU <germain.gau@gmail.com>
2019-02-04 21:27:32 +0100

    secrets directory parsing

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,6 +1,10 @@
 package agent
 
 import (
+	"os"
+	"os/user"
+	"path"
+
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -11,6 +15,18 @@ import (
 
 // AgentFlags unmarshall directly to the agent definition
 type AgentFlags = agent.Agent
+
+func getRSSHBaseDirectory() string {
+	user, err := user.Current()
+	if err != nil {
+		cwd, err := os.Getwd()
+		if err != nil {
+			return "/etc/rssh"
+		}
+		return cwd
+	}
+	return path.Join(user.HomeDir, ".rssh")
+}
 
 // NewCommand return the agent entrypoint command
 func NewCommand(flags *AgentFlags) *cobra.Command {
@@ -29,7 +45,7 @@ func NewCommand(flags *AgentFlags) *cobra.Command {
 		&flags.SecretsDirectory,
 		"secrets-dir",
 		"s",
-		"",
+		path.Join(getRSSHBaseDirectory(), "identities"),
 		"Directory used to store secret keys",
 	)
 	viper.BindPFlag("agent.secrets_directory", cmd.PersistentFlags().Lookup("secrets-dir"))
